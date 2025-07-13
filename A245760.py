b7d9d424f36c7719780eae4d8eb6be0ce1d00338
Brendan Gimby <bgimby@umich.edu>
2025-07-13 00:15:48 -0600

    Cleanup script, fix record breaker csv

--- A245760.py
+++ A245760.py
@@ -1,13 +1,16 @@
-import cudf
+import multiprocessing
 import numpy
 import time
 import pandas
 import sys
+import os
 import numba
 
-START, END = int(sys.argv[-2]), int(sys.argv[-1])
+BLOCKSIZE = 1_000_000
+CONCURRENCY = 8
 
 def maximalPersistenceBase(x):
+    """Find the absolute persistence and maximal base of x"""
     max_per = 1
     max_base = 2
     base = 2
@@ -42,24 +45,64 @@ def top32(x):
 def bottom32(x):
     return x & (2 ** 32 - 1)
 
-starttime = time.time()
-print("Calculating persistences and bases")
-sr = cudf.Series(range(START, END), dtype="uint32")
-pers_and_bases_combined = sr.apply(maximalPersistenceBase)
-print(f"Done, took {time.time() - starttime}s")
+def generate_series(start, end, filename):
+    """Generate the absolute persistence and maximal base for a range of integers
+
+    Dumps integer, absolute persistence, and maximal base into a csv called `filename`.
+    Covers the range start <= n < end.
+    Works incrementally, generating `BLOCKSIZE` rows at a time.
+    """
+    import cudf
+    starttime = time.time()
+    blockend = min(start+BLOCKSIZE, end)
+    print(f"Calculating persistences and bases for range {start}, {blockend}")
+    sr = cudf.Series(range(start, blockend), dtype="uint32")
+    pers_and_bases_combined = sr.apply(maximalPersistenceBase)
+    print(f"Done, took {time.time() - starttime}s")
+
+    starttime = time.time()
+    print("Splitting persistences from bases")
+    persistences_and_bases = pandas.DataFrame({
+        "Integer": sr.to_pandas(), 
+        "Persistence": pers_and_bases_combined.to_pandas().apply(top32),
+        "Base": pers_and_bases_combined.to_pandas().apply(bottom32),
+    })
+    print(f"Done, took {time.time() - starttime}s")
+
+    print("Dumping to file")
+    starttime = time.time()
+    persistences_and_bases.to_csv(filename, index=False, mode='a', header=not os.path.exists(filename))
+    print(f"Done, took {time.time() - starttime}s")
+    if blockend < end:
+        generate_series(blockend, end, filename)
+
+def generate_series_parallel__impl(start, end, stepsize, offset):
+    import cudf
+    sr = cudf.Series(range(start+offset, end, stepsize), dtype="uint32")
+    pers_and_bases_combined = sr.apply(maximalPersistenceBase)
+    return pandas.DataFrame({
+        "Integer": sr.to_pandas(), 
+        "Persistence": pers_and_bases_combined.to_pandas().apply(top32),
+        "Base": pers_and_bases_combined.to_pandas().apply(bottom32),
+    })
+
+def generate_series_parallel(start, end, filename, concurrency=CONCURRENCY, blocksize=BLOCKSIZE):
+    starttime = time.time()
+    blockend = min(start+blocksize*concurrency, end)
+    print(f"Calculating persistences and bases for range {start}, {blockend}")
+    with multiprocessing.Pool(concurrency) as p:
+        inputs = [(start, blockend, concurrency, offset) for offset in range(concurrency)]
+        ps_and_bases_list = p.starmap(generate_series_parallel__impl, inputs)
+        df = pandas.concat(ps_and_bases_list).sort_values(by="Integer")
+    print(f"Done, took {time.time() - starttime}s")
+    print("Dumping to file")
+    starttime = time.time()
+    df.to_csv(filename, index=False, mode='a', header=not os.path.exists(filename))
+    print(f"Done, took {time.time() - starttime}s")
+    if blockend < end:
+        generate_series_parallel(blockend, end, filename, concurrency, blocksize)
 
 
-starttime = time.time()
-print("Splitting persistences from bases")
-persistences_and_bases = pandas.DataFrame({
-    "Integer": sr.to_pandas(), 
-    "Persistence": pers_and_bases_combined.to_pandas().apply(top32),
-    "Base": pers_and_bases_combined.to_pandas().apply(bottom32),
-})
-print(f"Done, took {time.time() - starttime}s")
-
-
-print("Dumping to file")
-starttime = time.time()
-persistences_and_bases.to_csv(f"{START}-{END}.csv", index=False)
-print(f"Done, took {time.time() - starttime}s")
+if __name__ == "__main__":
+    START, END = int(sys.argv[-2]), int(sys.argv[-1])
+    generate_series(START, END, f"START-END.csv")
